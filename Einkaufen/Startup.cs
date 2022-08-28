24d4e4e08f3e6efc37d33c51b91b319329dc0345
Niroj <nirojkumarsahoo55@gmail.com>
2022-08-28 09:47:03 +0530

    First commit

---
+++ Einkaufen/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Einkaufen.Startup))]
+namespace Einkaufen
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
